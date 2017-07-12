4336300264038493b55b15d87b89219716ccea09
Dmytro <plekhotkindmytro@gmail.com>
2017-07-12 22:58:34 +0300

    Add sounds

--- scripts.js
+++ scripts.js
@@ -29,8 +29,48 @@ var gameState = {
 };
 
 $(document).ready(function () {
-    $(".play-tiles").on("click", function () {});
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+    var context = new AudioContext(),
+
+        masterGain = context.createGain();
+    nodes = [];
+
+    masterGain.gain.value = 0.3;
+    masterGain.connect(context.destination);
+
+    $(".play-tile").on("mousedown", function () {
+        var oscillator = context.createOscillator();
+        oscillator.type = 'square';
+        oscillator.frequency.value = 760;
+        oscillator.connect(masterGain);
+        oscillator.start(0);
+
+        nodes.push(oscillator);
+
+    });
+
+    $(".play-tile").on("mouseup", function () {
+        var new_nodes = [];
+
+        for (var i = 0; i < nodes.length; i++) {
+            if (Math.round(nodes[i].frequency.value) === Math.round(760)) {
+                nodes[i].stop(0);
+                nodes[i].disconnect();
+            } else {
+                new_nodes.push(nodes[i]);
+            }
+        }
+
+        nodes = new_nodes;
+
+    });
+
+
 });
+
+
+
 
 // on - activates display #output
 //    - makes controll buttons enabled
